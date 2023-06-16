c9bb8d26d53a703855032170a8d69c0fe78123de
Gustavo Carvalho <gcrodrigues413@gmail.com>
2023-06-15 23:00:25 -0300

    refactor: move input field to packages folder

---
+++ packages/input/storybook/argTypes.ts
@@ -0,0 +1,68 @@
+import { ArgTypes } from '@storybook/react'
+import { InputFieldProps } from '../src/types'
+
+export const argTypes: Partial<ArgTypes<InputFieldProps>> = {
+	disabled: {
+		table: {
+			type: { summary: 'bool' },
+			defaultValue: { summary: false },
+		},
+		type: { name: 'boolean' },
+	},
+
+	error: {
+		table: {
+			type: { summary: 'string' },
+		},
+		type: { name: 'string' },
+	},
+
+	helper: {
+		table: {
+			type: { summary: 'string' },
+		},
+		type: { name: 'string' },
+	},
+
+	icon: {
+		table: {
+			type: { summary: ' ReactNode' },
+		},
+	},
+
+	label: {
+		table: {
+			type: {
+				summary: 'string',
+			},
+		},
+		type: { name: 'string' },
+	},
+
+	name: {
+		table: {
+			type: {
+				summary: 'string',
+			},
+		},
+		description:
+			'Ariakit Form component requires the input field to have a unique name',
+		type: { name: 'string', required: true },
+	},
+
+	placeholder: {
+		table: {
+			type: {
+				summary: 'string',
+			},
+		},
+		type: { name: 'string' },
+	},
+
+	type: {
+		table: {
+			type: { summary: 'string' },
+			defaultValue: { summary: 'text' },
+		},
+	},
+}
